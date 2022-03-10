6d70b8a6308678f4e26a6eb6ee9f91bb60c75185
nnnnullll <1454672993@qq.com>
2022-03-10 13:56:06 +0800

    test

--- src/views/CaseTable.vue
+++ src/views/CaseTable.vue
@@ -23,11 +23,6 @@
                 <el-table-column prop="subject" label="标题"></el-table-column>
                 <el-table-column label="状态" align="center">
                     <template #default="scope">
-                          <!-- { name: '标签一', type: '' },
-          { name: '标签二', type: 'success' },
-          { name: '标签三', type: 'info' },
-          { name: '标签四', type: 'warning' },
-          { name: '标签五', type: 'danger' } -->
                         <el-tag :type="tag.type">{{tag.name}}</el-tag>
                     </template>
                 </el-table-column>
